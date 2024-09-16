3780e9991748ddd0fd066abafeeaf6e4b9bd5b04
Jan Kejla <redisperko@seznam.cz>
2024-09-16 21:50:47 +0200

    app: finished the core of the app

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-def load_data(file):
+def load_file_into_DF(file):
     """Load data from a TSV file (uploaded through Streamlit)."""
     return pd.read_csv(file, sep='\t')
 
@@ -11,14 +13,51 @@ def display_data(df, custom_name):
     st.write(f"### Top five rows from: `{custom_name}`")
     st.dataframe(df)
 
-def plot_data(list_of_files_and_custom_names):
-    """Plot heatmaps for each uploaded file."""
-    for file, custom_name in list_of_files_and_custom_names:
-        st.write(custom_name)
-        st.write(file)
-    
+def apply_custom_colnames(list_of_DFs_and_custom_names):
+    """Changes the abundance column name to the custom colname for each DF in place."""
+    for df, colname in list_of_DFs_and_custom_names:
+        df.rename(columns={"abundance":colname}, inplace=True)
+
+def plot_data(list_of_DFs_and_custom_names):
+    """Plot heatmaps for each passed DF."""
+    names = [df_and_name[1] for df_and_name in list_of_DFs_and_custom_names]
+    first_df, first_name = list_of_DFs_and_custom_names[0]
+
+    first_df_grouped = first_df.groupby(by = "kingdom; phylum; class; order; family; genus; species").sum()
+
+    for index, name_and_DF in enumerate(list_of_DFs_and_custom_names):
+        # st.write(index)
+        # st.write(name_and_DF)
+        df, name = name_and_DF
+        if index == 0:
+            pass
+        else:
+            # group:
+            df = df.groupby(by = "kingdom; phylum; class; order; family; genus; species").sum()
+            
+            # left join to the first df:
+            first_df_grouped = first_df_grouped.merge(df, on="kingdom; phylum; class; order; family; genus; species", how = 'left')
+    #st.write(first_df_grouped[names])
+            
+    # # FINAL touches:        
+    # # ------------------------- #
+    merged_df = first_df_grouped
+    # sort values by the first sample:
+    merged_df.sort_values(by=first_name, ascending = False, inplace=True)
+
+    # crop for the top 25
+    final_df = merged_df[names][:25]
+
+    st.dataframe(final_df)
+
+    plt.figure(figsize=(6,15))
+    sns.heatmap(data = final_df, cmap = "coolwarm", annot = True)
+    st.pyplot(plt)
+
 def main():
+
     st.title("TSV Files Viewer")
+
 
     # Step 1: File selection
     uploaded_files = st.file_uploader(
@@ -27,8 +66,8 @@ def main():
         accept_multiple_files=True)
 
     if uploaded_files:
-        # Step 2: Create a list to store file and custom name associations
-        files_with_names = []
+        # Step 2: Create a list to store df and custom name associations
+        DFs_with_names = []
 
         # Display a row for each file with an input field for the name
         st.write("### Choose custom column names:")
@@ -36,23 +75,26 @@ def main():
             file_name = uploaded_file.name
             # Display input field with default value as the filename
             custom_name = st.text_input(f"Enter a name for the data in `{file_name}`:", value=file_name.split(".")[0], key=file_name)
-            # Append the file and its custom name to the list
-            files_with_names.append((uploaded_file, custom_name))
+            # Append the file's dataframe and its custom name to the list
+            DFs_with_names.append((load_file_into_DF(uploaded_file), custom_name))
 
         # debug:
         with st.sidebar:
             st.write("## Debug bar:")
-            st.write(files_with_names)
+            st.write("debugging:")
 
         # Step 3: Display the contents of each file as a dataframe with the custom name
-        for uploaded_file, custom_name in files_with_names:
-            df = load_data(uploaded_file)
+        for df, custom_name in DFs_with_names:
             display_data(df.head(), custom_name)
+        
+        # change the 'abundance' column into whatever we want this series to be called in the output -- the custom colname
+        apply_custom_colnames(DFs_with_names)
 
         # Step 4: Plot the contents of the files
         if st.button("Plot heatmap"):
             st.write("Graphs plotted!")
-            plot_data(files_with_names)
+            plot_data(DFs_with_names)
+            
     else:
         st.write("Please upload one or more .tsv files.")
 
