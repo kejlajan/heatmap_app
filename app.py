ff817c909473d9552428a3664aff99b20aa0e1d8
Jan Kejla <redisperko@seznam.cz>
2024-10-29 21:45:20 +0100

    changed the order of operations on DFs: group > sort > cut > join

--- app.py
+++ app.py
@@ -4,6 +4,31 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+cmap_keywords = [
+    "viridis",
+    "plasma",
+    "inferno",
+    "magma",
+    "cividis",
+    "coolwarm",
+    "jet",
+    "gray",
+    "spring",
+    "summer",
+    "autumn",
+    "winter",
+    "bone",
+    "copper",
+    "hsv",
+    "twilight",
+    "twilight_shifted",
+    "Set1",
+    "Set2",
+    "Set3",
+    "Dark2",
+    "Paired",
+]
+
 def load_file_into_DF(file):
     """Load data from a TSV file (uploaded through Streamlit)."""
     return pd.read_csv(file, sep='\t')
@@ -38,47 +63,57 @@ def split_hierarchy_column(DF, colname_with_hierarchy):
     return colname_with_hierarchy.split(';'), DF
 
 
-def plot_data(list_of_DFs_and_custom_names, hierarchy_selection, number_of_records):
+def plot_data(list_of_DFs_and_custom_names, hierarchy_selection, number_of_records, cmap_color):
     """Plot heatmaps for each passed DF."""
     names = [df_and_name[1] for df_and_name in list_of_DFs_and_custom_names]
     first_df, first_name = list_of_DFs_and_custom_names[0]
 
     first_df_grouped = split_hierarchy_column(first_df, get_hierarchy_colname(first_df))[1].groupby(by = hierarchy_selection).sum()
 
+    first_df_sorted = first_df_grouped.sort_values(by=first_name, ascending = False)[:number_of_records]
+
     for index, name_and_DF in enumerate(list_of_DFs_and_custom_names):
-        df, _ = name_and_DF
+        df, name = name_and_DF
         if index == 0:
-            pass
+            pass # ignore the first one
         else:
-            # group:
+            # group by hierarchy:
             df = split_hierarchy_column(df, get_hierarchy_colname(df))[1].groupby(by = hierarchy_selection).sum()
             
-            # left join to the first df:
-            first_df_grouped = first_df_grouped.merge(df, on = hierarchy_selection, how = 'outer')
+            # sort by abundance:
+            df.sort_values(by=name, ascending = False, inplace = True)
+
+            # cut:
+            df = df[:number_of_records]
+
+            # full join to the first df:
+            first_df_sorted = first_df_sorted.merge(df, on = hierarchy_selection, how = 'outer')
             
-    # # FINAL touches:        
-    # # ------------------------- #
-    merged_df = first_df_grouped
-    # sort values by the first sample:
-    merged_df.sort_values(by=first_name, ascending = False, inplace=True)
+    # get only the colnames that matter
+    final_df = first_df_sorted.sort_values(by = first_name, ascending = False)[names]
 
-    # show the top records only
-    final_df = merged_df[names][:number_of_records]
-    st.dataframe(final_df)
+    col1, col2 = st.columns(2)
 
-    plt.figure(figsize=(6,15))
-    sns.heatmap(data = final_df, cmap = "coolwarm", annot = True)
-    st.pyplot(plt)
+    with col1:
+        plt.figure(figsize=(6,15))
+        sns.heatmap(data = final_df, cmap = cmap_color, annot = True)
+        st.pyplot(plt)
+
+    with col2:
+        st.dataframe(final_df)
 
 def main():
 
     st.title("TSV Files Viewer")
 
     # Step 1: File selection
-    uploaded_files = st.file_uploader(
-        "Choose TSV files", 
-        type="tsv", 
-        accept_multiple_files=True)
+    col_file_select, col_filenames = st.columns(2)
+    
+    with col_file_select:
+        uploaded_files = st.file_uploader(
+            "Choose TSV files", 
+            type="tsv", 
+            accept_multiple_files=True)
     
     if uploaded_files:
         # Step 2: Create a list to store df and custom name associations
@@ -93,10 +128,13 @@ def main():
             # Append the file's dataframe and its custom name to the list
             DFs_with_names.append((load_file_into_DF(uploaded_file), custom_name))
 
+        st.write("---")
+
         # select a hierarchy level:
         with st.sidebar:
            hierarchy_selection = st.multiselect("Level", get_hierarchy_colname(DFs_with_names[0][0]).split(';'), " species", placeholder="Choose the level of aggregation")
            number_of_records = st.slider("How many top records from each file", 1, 100, 25)
+           selected_cmap = st.selectbox("Color scheme", cmap_keywords)
 
         # Step 3: Display the contents of each file as a dataframe with the custom name
         for df, custom_name in DFs_with_names:
@@ -105,10 +143,15 @@ def main():
         # change the 'abundance' column into whatever we want this series to be called in the output -- the custom colname
         apply_custom_colnames(DFs_with_names)
 
+        st.write("---")
+
         # Step 4: Plot the contents of the files
-        if st.button("Plot heatmap"):
-            st.write("Graphs plotted!")
-            plot_data(DFs_with_names, hierarchy_selection, number_of_records)
+        with st.sidebar:
+            plot_heatmap = st.button("Plot heatmap")
+                
+        if plot_heatmap:        
+            st.write("Heatmap plotted!")
+            plot_data(DFs_with_names, hierarchy_selection, number_of_records, selected_cmap)
             
     else:
         st.write("Please upload one or more .tsv files.")
